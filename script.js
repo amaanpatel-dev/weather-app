660da8b65effd997be8ad028fe36f8cbc4700059
amaanpatel-dev <amaanweb32@gmail.com>
2025-01-02 20:12:52 +0530

    Add Enter key event listener to trigger seaarch

--- script.js
+++ script.js
@@ -10,20 +10,35 @@ document.addEventListener("DOMContentLoaded", () => {
   const weatherIconDisplay = document.querySelector(".weather-icon");
 
   submitBtn.addEventListener("click", async (e) => {
-      const city = cityInput.value.trim();
-      cityInput.value = ''
+    const city = cityInput.value.trim();
+    cityInput.value = "";
     if (!city) return;
 
-    try{
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+
+  cityInput.addEventListener("keydown", async (e) => {
+    if (e.key === "Enter") {
+      const city = cityInput.value.trim();
+      cityInput.value = "";
+      if (!city) return;
+
+      try {
         const weatherData = await fetchWeatherData(city);
-        displayWeatherData(weatherData)
-    }catch (error){
-        showError()
+        displayWeatherData(weatherData);
+      } catch (error) {
+        showError();
+      }
     }
   });
 
   async function fetchWeatherData(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -36,31 +51,32 @@ document.addEventListener("DOMContentLoaded", () => {
   function displayWeatherData(data) {
     console.log(data);
 
-    const { name, main, wind, weather } = data 
-    cityDisplay.textContent = name
-    temperatureDisplay.textContent = `${Math.round(main.temp)}°C`
-    feelslikeDisplay.textContent = `Feels like ${Math.round(main.feels_like)}°C`
-    humidityDisplay.textContent = `${main.humidity}%`
-    windDisplay.textContent = `${wind.speed} km/h`
+    const { name, main, wind, weather } = data;
+    cityDisplay.textContent = name;
+    temperatureDisplay.textContent = `${Math.round(main.temp)}°C`;
+    feelslikeDisplay.textContent = `Feels like ${Math.round(
+      main.feels_like
+    )}°C`;
+    humidityDisplay.textContent = `${main.humidity}%`;
+    windDisplay.textContent = `${wind.speed} km/h`;
 
-    if(weather[0].main == "Rain"){
-        weatherIconDisplay.src ="./images/rain.png"
-    }else if(weather[0].main == "Clouds"){
-        weatherIconDisplay.src ="./images/clouds.png"
-    }else if(weather[0].main == "Drizzle"){
-        weatherIconDisplay.src = "./images/drizzle.png"
-    }else if(weather[0].main == "Clear"){
-        weatherIconDisplay.src ="./images/clear.png"
-    }else if(weather[0].main == "Mist"){
-        weatherIconDisplay.src ="./images/mist.png"
-    }else if(weather[0].main == "Snow"){
-        weatherIconDisplay.src ="./images/snow.png"
+    if (weather[0].main == "Rain") {
+      weatherIconDisplay.src = "./images/rain.png";
+    } else if (weather[0].main == "Clouds") {
+      weatherIconDisplay.src = "./images/clouds.png";
+    } else if (weather[0].main == "Drizzle") {
+      weatherIconDisplay.src = "./images/drizzle.png";
+    } else if (weather[0].main == "Clear") {
+      weatherIconDisplay.src = "./images/clear.png";
+    } else if (weather[0].main == "Mist") {
+      weatherIconDisplay.src = "./images/mist.png";
+    } else if (weather[0].main == "Snow") {
+      weatherIconDisplay.src = "./images/snow.png";
     }
-
   }
 
   function showError() {
-   alert("City not found")
-   cityInput.value = '';
+    alert("City not found");
+    cityInput.value = "";
   }
 });
